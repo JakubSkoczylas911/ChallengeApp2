d126c56c4f79eb7188e9c3ac1bea1dbcc1c5aeea
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-03-27 20:49:34 +0100

    Zadanie domowe dzień 6

---
+++ ConsoleApp2/ConsoleApp2/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    public class Employee
+    {
+        private string name;
+        private string surname;
+        public int age;
+        public int score;
+        public Employee(string name, string surname, int age)
+        {
+
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.score = 0;
+
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public void AddScore(int number)
+        {
+            this.score = this.score + number;
+
+            this.score += number;
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp2;
+
+Employee employee1 = new Employee("Renata", "Skoczylas", 57);
+Employee employee2 = new Employee("Marcin", "Skoczylas", 43);
+Employee employee3 = new Employee("Jakub", "Skoczylas", 39);
+employee1.AddScore(10);
+employee1.AddScore(5);
+employee1.AddScore(1);
+employee1.AddScore(5);
+employee1.AddScore(2);
+
+employee2.AddScore(3);
+employee2.AddScore(6);
+employee2.AddScore(7);
+employee2.AddScore(8);
+employee2.AddScore(9);
+
+employee3.AddScore(8);
+employee3.AddScore(4);
+employee3.AddScore(4);
+employee3.AddScore(10);
+employee3.AddScore(10);
+List<Employee> employees = new List<Employee>()
+{
+    employee1 , employee2,employee3
+    };
+int maxScore = -1;
+Employee employeeWithMaxScore = null;
+
+foreach (var employee in employees)
+{
+    if (employee.score > maxScore)
+    {
+        employeeWithMaxScore = employee;
+    }
+}
+
+Console.WriteLine($" (Najlepszy wynik) - {employeeWithMaxScore.Name} {employeeWithMaxScore.Surname}, z Wynikiem {employeeWithMaxScore.score}!");
+
+
