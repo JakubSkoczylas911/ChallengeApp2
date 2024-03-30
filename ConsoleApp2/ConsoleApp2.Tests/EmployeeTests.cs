4e2f6806958e61d1ee1ae8c2a29dd17d42e77ee6
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-03-30 16:50:55 +0100

    Zadanie domowe 1 podejście

---
+++ ConsoleApp2/ConsoleApp2.Tests/EmployeeTests.cs
@@ -0,0 +1,21 @@
+namespace ConsoleApp2.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+{
+
+            var employee = new Employee("Renata", "Skoczylas", 57);
+            employee.AddScore(5);
+            employee.AddScore(6);
+
+
+            var result = employee.Result;
+
+
+            Assert.AreEqual(11, result);
+
+        }
+    }
+}
