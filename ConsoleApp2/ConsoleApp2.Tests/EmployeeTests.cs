67765ae2ce3c68efcef1d7a85fa64323e55e5a77
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-04-23 17:13:18 +0200

    Przygotowana klasa Employee.cs po liczenie statystyk

--- ConsoleApp2/ConsoleApp2.Tests/EmployeeTests.cs
+++ ConsoleApp2/ConsoleApp2.Tests/EmployeeTests.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2.Tests
             var employee = new Employee("Renata", "Skoczylas", 57);
             employee.AddScore(5);
             employee.AddScore(6);
- var result = employee.score;
+ var result = employee.Score;
  Assert.AreEqual(11, result);
 }
     }
--- ConsoleApp2/ConsoleApp2/Employee.cs
+++ ConsoleApp2/ConsoleApp2/Employee.cs
@@ -1,27 +1,49 @@
-﻿namespace ConsoleApp2
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp2
 {
     public class Employee
     {
+        private List<float> scores = new List<float>();
+
         private string name;
         private string surname;
         public int age;
         public int score;
         public Employee(string name, string surname, int age)
         {
-
             this.Name = name;
             this.Surname = surname;
             this.Age = age;
             this.score = 0;
-
         }
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
 
-        public void AddScore(int number)
+        public void AddScore(float score)
         {
-            this.score += number;
+            this.scores.Add(score);
         }
-    }
+        public Statistics GetStatistics()
+        { 
+        var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            foreach (var score in this.scores)
+            {
+                statistics.Max = Math.Max(statistics.Max, score);
+                statistics.Min = Math.Min(statistics.Min, score);
+                statistics.Average += score;
 }
+            statistics.Average = statistics.Average / this.scores.Count;
+
+        return statistics ;
+            }
+}
+}
+
+
+
+    
