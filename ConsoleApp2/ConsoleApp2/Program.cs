c3c5ddf63edf9beb7da99b70ba8c9edc4fb381d8
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-04-29 12:18:16 +0200

    Poprawiona klasa "Program.cs" by dodawane punkty były identyczne jak w testach

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,17 +1,17 @@
 ﻿using ConsoleApp2;
 
-var employee1 = new Employee("Renata", "Skoczylas", 57);
+var employee = new Employee("Renata", "Skoczylas", 57);
 
-employee1.AddScore(10);
-employee1.AddScore(5);
-employee1.AddScore(1);
-employee1.AddScore(5);
-employee1.AddScore(2);
-var statistics = employee1.GetStatistics();
-Console.WriteLine($"Pracownik:{employee1.Name} {employee1.Surname}");
+employee.AddScore(5);
+employee.AddScore(10);
+
+employee.AddScore(6);
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Pracownik:{employee.Name} {employee.Surname}");
 Console.WriteLine($"Średnia ocen:{statistics.Average:N2}");
 Console.WriteLine($"Najniższa ocena:{statistics.Min:N2}");
 Console.WriteLine($"Najwyższa ocena:{statistics.Max:N2}");
+
 
 
 
