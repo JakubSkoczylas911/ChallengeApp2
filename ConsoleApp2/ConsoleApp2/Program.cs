fea4108aba4d7336cc0ea5a10e6506a5ccff2878
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-06-18 15:53:48 +0200

    Działająca klasa "Program.cs" do zadania domowego z dnia 11

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,10 +7,26 @@ employee.AddScore(10);
 
 employee.AddScore(6);
 var statistics = employee.GetStatistics();
+var statistics1 = employee.GetStatisticsWithFor();
+var statistics2 = employee.GetStatisticsWithDoWhile();
+var statistics3 = employee.GetStatisticsWithWhile();
+Console.WriteLine("Statistics with method Foreach");
 Console.WriteLine($"Pracownik:{employee.Name} {employee.Surname}");
 Console.WriteLine($"Średnia ocen:{statistics.Average:N2}");
 Console.WriteLine($"Najniższa ocena:{statistics.Min:N2}");
 Console.WriteLine($"Najwyższa ocena:{statistics.Max:N2}");
+Console.WriteLine("Statistics with method For");
+Console.WriteLine($"Średnia ocen:{statistics.Average}");
+Console.WriteLine($"Najniższa ocena:{statistics.Min}");
+Console.WriteLine($"Najwyższa ocena:{statistics.Max}");
+Console.WriteLine("Statistics with method Do While");
+Console.WriteLine($"Średnia ocena:{statistics.Average:N2}");
+Console.WriteLine($"Najwyższa ocena:{statistics.Max}");
+Console.WriteLine($"Najniższa ocena:{statistics.Min}");
+Console.WriteLine("Statistics with method While");
+Console.WriteLine($"Średnia ocen:{statistics.Average:N2}");
+Console.WriteLine($"Najwyższa ocena:{statistics.Max}");
+Console.WriteLine($"Najniższa ocena:{statistics.Min}");
 
 
 
