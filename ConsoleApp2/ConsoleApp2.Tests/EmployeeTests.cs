71d53844df6ea5a1a413625767f2d6e679d0a2a8
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2024-07-03 11:09:11 +0200

    Test do zadania domowego z dnia 12

--- ConsoleApp2/ConsoleApp2.Tests/EmployeeTests.cs
+++ ConsoleApp2/ConsoleApp2.Tests/EmployeeTests.cs
@@ -3,38 +3,18 @@ namespace ConsoleApp2.Tests
     public class Tests
     {
         [Test]
-        public void WhenEmployeHighestScoreisStatisticsMax()
+        public void IfAverageCountedByGivenScoresInNumbersAverageIsCorrectAverageLetter()
         {
 
-            var employee = new Employee("Renata", "Skoczylas", 57);
-            employee.AddScore(5);
+            var employee = new Employee();
             employee.AddScore(10);
+            employee.AddScore(15);
+            employee.AddScore(25);
+            employee.AddScore(35);
 
-            employee.AddScore(6);
-            var statistics = employee.GetStatistics();
-            Assert.AreEqual(10, statistics.Max);
-        }
-        [Test]
-        public void WhenEmployeeLowestScoreisStatisticsMin()
-        {
-            var employee = new Employee("Renata", "Skoczylas", 57);
-            employee.AddScore(5);
-            employee.AddScore(10);
 
-            employee.AddScore(6);
             var statistics = employee.GetStatistics();
-            Assert.AreEqual(5, statistics.Min);
-        }
-        [Test]
-        public void IfAverageFromCalculatorIsStatisticsAverage()
-        {
-            var employee = new Employee("Renata", "Skoczylas", 57);
-            employee.AddScore(5);
-            employee.AddScore(10);
-
-            employee.AddScore(6);
-            var statistics = employee.GetStatistics();
-            Assert.AreEqual(7, statistics.Average);
+            Assert.AreEqual(statistics.AverageLetter, 'D');
         }
     }
 }
